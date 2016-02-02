256c845a95240521c97730b987184b9182ec3882
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-02-02 23:58:43 +0200

    add 4.1 prometh. blank

---
+++ pr_test4.1.py
@@ -0,0 +1,10 @@
+# palindrom
+import sys
+inp_string = str(sys.argv[1])
+r = None
+print inp_string
+y = len (inp_string)
+print y
+for letter in inp_string:
+    print letter
+#print inp_string.replace("w", "vv")
