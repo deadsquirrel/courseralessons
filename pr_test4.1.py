2c1bf5726a2bc0bc7119c69fb902320c19b52ac7
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-02-03 11:32:52 +0200

    modified 4.1

--- pr_test4.1.py
+++ pr_test4.1.py
@@ -1,10 +1,12 @@
 # palindrom
 import sys
 inp_string = str(sys.argv[1])
-r = None
+
 print inp_string
-y = len (inp_string)
-print y
-for letter in inp_string:
-    print letter
-#print inp_string.replace("w", "vv")
+
+change = inp_string[::-1]
+print change
+
+if inp_string == change:
+    print "YES"
+else: print "NO"
