9b64897575928ccba21ccef6b4fcbf2f345eb696
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-02-03 15:36:15 +0200

    done 4.1 prometheus assign.

--- pr_test4.1.py
+++ pr_test4.1.py
@@ -1,8 +1,18 @@
 # palindrom
 import sys
 inp_string = str(sys.argv[1])
-
+#r = None
 print inp_string
+#y = len (inp_string)
+
+inp = inp_string.lower()
+inp_string = inp.replace(" ", "")
+
+print "Out:",inp_string
+    
+    
+#print inp_string.replace("w", "vv")
+
 
 change = inp_string[::-1]
 print change
@@ -10,3 +20,5 @@ print change
 if inp_string == change:
     print "YES"
 else: print "NO"
+
+
