07133bbf0be972045b8f2d186e82489fa4216ca0
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-02-07 00:44:57 +0200

    clean up 10.2

--- test10.2.py
+++ test10.2.py
@@ -2,13 +2,10 @@
  You can pull the hour out from the 'From ' line by finding the time and 
  then splitting the string a second time using a colon. 
 '''
-
+    
 name = raw_input("Enter file:")
-if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
-val = 0
 count = {}
-print "==",count
 for line in handle:
     if not line.startswith('From '): continue
     else:
@@ -19,12 +16,12 @@ for line in handle:
             d2 = {keyt: count[keyt] + 1}
         else:
             d2 = {keyt: 1}
-            count.update(d2)
-print "Count:",count
+        count.update(d2)
+#print "Count:",count
             
 list_keys = count.keys()
 list_keys.sort()
-print "List: ",list_keys
+#print "List: ",list_keys
 
 for j in list_keys:
     print j, count[j]
