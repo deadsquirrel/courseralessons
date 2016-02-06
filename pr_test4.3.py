0c2b3480fb81258b6d22335d7f5e9915b63982c8
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-02-06 21:29:39 +0200

    done 4.3 prometheus assign.

--- pr_test4.3.py
+++ pr_test4.3.py
@@ -4,15 +4,22 @@ import sys
 inp = sys.argv[1]
 opn = 0
 cls = 0
+y = 0
+count = 0
 
-
-for i in inp:
-    if i == "(":
-        opn = opn + 1
-    elif i == ")":
-        cls = cls + 1
-    else: print "error"
-    
-if opn - cls == 0:
+while count < len(inp):
+    count = count + 1
+    for i in inp:
+        if y < 0 or cls > opn:
+            break
+        elif i == "(":
+            opn = opn + 1
+            y = opn - cls
+        elif i == ")":
+            cls = cls + 1
+            y = opn - cls
+        else: print "error"
+if y == 0:
     print "YES"
 else: print "NO"
+        
