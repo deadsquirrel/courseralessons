1b46e9896476361798fe655fbefb54168a4c9733
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-02-07 21:32:54 +0200

    add module_test_1 prometheus

---
+++ pr_mod1.py
@@ -0,0 +1,44 @@
+'''
+Decrypt message
+Bacon's cipher
+'''
+import sys
+inp = sys.argv[1]
+key = 'aaaaabbbbbabbbaabbababbaaababaab'
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+n = 0
+k = 5
+key_v = []
+newl = []
+
+'''
+create list of tuple with key_value_structure = key_letter_of_alphabet
+'''
+for i in alphabet:
+    y = (i, key[n:k])
+#    print "n:",n,"y:", y
+    key_v.append(y)
+    n = n + 1
+    k = k + 1
+
+#print key_v
+
+'''
+a.islower() .
+a.isupper(), b.isupper(), c.isupper(), d.isupper()
+True False
+ "a" or "b"
+'''
+print inp
+for j in inp:
+    if j == " ":
+        newl.append(j)
+    else:
+        low = j.islower()
+        if low == True:
+            newl.append('a')
+        else:
+            newl.append('b')
+
+print "------------"      
+print newl
