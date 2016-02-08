74def81632f479e463808c72bd6e8b7c892054ae
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-02-08 22:21:19 +0200

    done Decrypt message Bacon's ciper

--- pr_mod1.py
+++ pr_mod1.py
@@ -12,16 +12,14 @@ key_v = []
 newl = []
 s = str()
 '''
-create list of tuple with key_value_structure = key_letter_of_alphabet
+create list of tuples with key_value_structure = key_letter_of_alphabet
 '''
 for i in alphabet:
     y = (i, key[n:k])
-#    print "n:",n,"y:", y
     key_v.append(y)
     n = n + 1
     k = k + 1
 
-print key_v
 
 '''
 a.islower() .
@@ -29,10 +27,9 @@ a.isupper(), b.isupper(), c.isupper(), d.isupper()
 True False
  "a" or "b"
 '''
-print inp
+#print inp
 for j in inp:
     if j == " ":
-#        newl.append(j)
         continue
     else:
         low = j.islower()
@@ -41,21 +38,14 @@ for j in inp:
         else:
             newl.append('b')
 
-print "------------"      
-print newl
-print "------------"      
 newstr = ''.join(newl)
-print newstr
 counter = len(newl)
 while counter > 0:
-    print "N",counter 
     word = newstr[0:5]
-    print "word:",word
     for k, v in key_v:
         if v == word:
             lit = k
             s = s + lit
     newstr = newstr[5:]
-    print "shortline::",newstr
     counter = counter - 5
 print s
