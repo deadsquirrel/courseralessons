625e06a293664226c48360a76bbdec8b5a525f2b
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-02-22 23:36:22 +0200

    test5.1 in process

--- pr_test5.1.py
+++ pr_test5.1.py
@@ -7,21 +7,25 @@ but does not repeat elements.
 '''
 
 import sys
+list4 = [12,9,80,8,7]
+#inp_list = sys.argv[1]
 
-inp_list = sys.argv[1]
 
-def clean_list(inp_list):
-    for i in inp_list:
-        cheklist(i, inp_list(:i))
-        
 def cheklist (arg1, arg2):
     for j in arg2:
         if arg1 != j:
-        return arg2.append(arg1)
-        print "A"
+            return arg2.append(arg1)
+            print "A"
         else:
-            return print arg2
+            return arg2
+            print "B"
+        
+list = []
+for i in list4:
+    print list.append(i)
 
+    #    cheklist(i, inp_list(:i))
+    
 ''' example:
 def text_prompt(msg):
     try:
