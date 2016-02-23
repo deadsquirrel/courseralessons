96240c9a19d77a3f479c7308571580c895a3d60e
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-02-23 17:21:22 +0200

    find index of list

--- 5.1.py
+++ 5.1.py
@@ -2,11 +2,31 @@ import sys
 inp_list = sys.argv[1]
 #inp_list = [9,6,6,4,4,30]
 out_list = []
+next_list = []
 
-
-print ":",out_list
+print "inp:",inp_list
+print "out0:",out_list
 for i in inp_list:
-    j = out_list.append(i)
-print j 
-    
-#List.remove(
+    if i.isdigit():
+        out_list.append(i)
+print out_list
+for i in out_list:
+    print i
+    print "index", out_list.index(i)
+'''
+    if i != j:
+        print i, j, "a"
+    else: print "b"
+    #   next_list.append(i)
+        
+print next_list
+'''
+
+'''
+print "inp:",inp_list
+print "out0:",out_list
+for i in inp_list:
+    if i.isdigit():
+        out_list.append(i)
+print out_list
+'''
