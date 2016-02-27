80d67d27be8bb9c14d4a727d02d2dc675c4bce0b
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-02-27 22:53:48 +0200

    promet.assign. 5.1. v2 begin

---
+++ pr_test5.1_v2.py
@@ -0,0 +1,18 @@
+''' 
+create fun clean_list(list_to_clean
+that takes one argument - list any values (strings, integer and real numbers)
+of arbitrary length, and returns a list that consists of the same values, 
+but does not repeat elements.
+
+'''
+
+import sys
+import my_module_check_list
+
+inp_list = sys.argv[1]
+
+my_module_check_list.hi()
+
+my_module_check_list.clean_list(inp_list)
+
+my_module_check_list.checklist(n)
