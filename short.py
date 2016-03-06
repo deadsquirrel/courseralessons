7fa98a9e23eade0a3a922796d09c78f415641101
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-03-06 02:39:02 +0200

    check list

---
+++ short.py
@@ -0,0 +1,22 @@
+import sys
+import short_def 
+
+
+inp = sys.argv[1]
+
+
+print (short_def.ter(inp))
+
+
+
+
+'''
+example:
+mport mydef
+import sys
+
+n = sys.argv[1]
+
+mydef.hello()
+print (mydef.fib(int(n)))
+'''
---
+++ short_def.py
@@ -0,0 +1,20 @@
+def ter(a):
+    for i in a:
+        b = a[:]
+            if int(j) == int(i):
+                list.append
+    return counter
+
+
+# исключающая итерация — например, вывести элементы 1-го списка, которых нет во 2-м списке:
+#
+#  for item in set(L).difference(L2)
+
+'''
+example:
+def fib(n):
+    a = b = 1
+    for i in range(n - 2):
+        a, b = b, a + b
+    return b
+'''
