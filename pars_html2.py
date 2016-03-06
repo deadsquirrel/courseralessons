96c3e934c2b467485c3640a1e8a2afeb3d8e9984
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-03-06 22:19:51 +0200

    done

--- pars_html2.py
+++ pars_html2.py
@@ -10,21 +10,31 @@ count = int(raw_input('Enter count: '))
 pos = int(raw_input('Enter position: '))
 name = None
 # Retrieve all of the anchor tags
-
 while count > 0:
+    c_pos = pos
     name_list = []
     html = urllib.urlopen(url).read()
     soup = BeautifulSoup(html)
     tags = soup('a')
     for tag in tags:
-        print tag.get('href', None)
-        print 'names', tag.contents[0]
+#        print tag.get('href', None)
+#        print 'names', tag.contents[0]
         name_list.append(tag.get('href', None))
-
+        if c_pos == 1:
+            name = tag.contents[0]
+            print "//////////-=-=-=-=-=-", name
+        c_pos = c_pos - 1
     count = count - 1
     
     url = name_list[pos-1]
-#    name = url.contents[0]
     print 'count:', count, '---url---', url
+#name = soup.head.parent.parent.__class__.__name__
+#name = tag.string
 print '===', name
+
+
+'''
+http://python-data.dr-chuck.net/known_by_Fikret.html
+
+'''
    
