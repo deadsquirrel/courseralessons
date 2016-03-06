fefb212fabf0a09a0e105a3a2dbe4c728e0b52ca
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-03-06 20:36:13 +0200

    get all position in list - out

--- pars_html2.py
+++ pars_html2.py
@@ -5,11 +5,23 @@
 import urllib
 from BeautifulSoup import *
 
-url = raw_input('Enter - ')
+url = raw_input('Enter URL: ')
+count = int(raw_input('Enter count: '))
+pos = int(raw_input('Enter position: '))
 html = urllib.urlopen(url).read()
 soup = BeautifulSoup(html)
 
+name_list = ['A']
+N = 1
 # Retrieve all of the anchor tags
 tags = soup('a')
 for tag in tags:
-    print tag.get('href', None)
+#    print tag.get('href', None)
+#    print tag.contents[0]
+    name_list.append(tag.contents[0])
+print name_list
+while count > 0:
+    for name in name_list:
+        print name_list[(pos)*N]
+        count = count - 1
+        N += 1
