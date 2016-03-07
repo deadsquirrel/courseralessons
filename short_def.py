34eb02d02638d6377fbecb4ed64875b388d11829
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-03-07 23:58:11 +0200

    done. check list1 difference list2

--- short_def.py
+++ short_def.py
@@ -1,32 +1,9 @@
-def ter(a, b):
+def ter (a, b):
     list_a = []
-    for j in b:
-        for i in a:
-            print "i=",i, " and j=",j
-            list_a.append(i)
-            if i == j:
-                n = a.index(i)
-                print "n=",n
-                print a
-#                list_a.append(i)
-                print 'list', list_a
-                list_a.pop(n)
-            print a
+    for item in set(a).difference(b):
+        print item
+        list_a.append(item)
+        print a
+        print b
+    print "!",list_a
     return list_a
-
-    
-
-#  for item in set(L).difference(L2)
-
-'''
-example:
-def fib(n):
-    a = b = 1
-    for i in range(n - 2):
-        a, b = b, a + b
-    return b
-'''
-
-'''
-for item in set(L).difference(L2)
-'''
