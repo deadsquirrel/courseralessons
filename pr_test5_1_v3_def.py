83ae2fb118352277450327b06aa9fec363fe46e9
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-03-08 23:52:46 +0200

    difference item in list

--- pr_test5_1_v3_def.py
+++ pr_test5_1_v3_def.py
@@ -1,9 +1,10 @@
 def counter (a, b):
     list_a = []
-    for item in set(a).difference(b):
-        print item
+    for item in set(b).difference(a):
+#        print item
         list_a.append(item)
-        print a
-        print b
-    print "!",list_a
+#        print a
+#        print b
+#    print "!",list_a
+#    for i in set(b).difference(list_a)
     return list_a
