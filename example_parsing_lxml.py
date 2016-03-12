673acd3692e1a7eeb0bc0627bc9069f17a049e01
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-03-12 23:40:58 +0200

    try-except Element Tree

--- example_parsing_lxml.py
+++ example_parsing_lxml.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
-import lxml.etree                                                               
+
+try:
+    from lxml import etree
+except ImportError:
+    import xml.etree.ElementTree as etree
+
+#import lxml.etree
 tree = lxml.etree.parse('examples/feed.xml')
 tree.findall('//{http://www.w3.org/2005/Atom}*[@href]')
 tree.findall("//{http://www.w3.org/2005/Atom}*[@href='http://diveintomark.org/']")
-tree.findall('//{NS}author[{NS}uri]'.format(NS=NS)) 
+tree.findall('//{NS}author[{NS}uri]'.format(NS=NS))
