f2e00579228fd3b1cb2236a1def2a0b384582fb2
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-03-14 01:53:13 +0200

    clean debug printing

--- test13.1.py
+++ test13.1.py
@@ -9,14 +9,14 @@ data = uh.read()
 
 tree = ET.fromstring(data)
 counts = tree.findall('.//comment')
-print 'my count:', counts
-print 'my count len:', len(counts)
+#print 'my count:', counts
+#print 'my count len:', len(counts)
 
 sum = 0
 for item in counts:
-    print item
-    print 'Name', item.find('name').text
-    print 'Count', item.find('count').text
+#    print item
+#    print 'Name', item.find('name').text
+#    print 'Count', item.find('count').text
     count_n = int(item.find('count').text)
     sum = sum + count_n
 
