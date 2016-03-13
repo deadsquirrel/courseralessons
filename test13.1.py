5dcf3d8c3cf8486d1b48f61642cb95abca8f7ef9
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-03-14 01:45:57 +0200

    with errors

--- test13.1.py
+++ test13.1.py
@@ -1,25 +1,18 @@
 import urllib
 import xml.etree.ElementTree as ET
 
-serviceurl = 'http://maps.googleapis.com/maps/api/geocode/xml?'
+url = raw_input('Enter location: ')
+print 'Retrieving', url
+uh = urllib.urlopen(url)
 
-while True:
-    address = raw_input('Enter location: ')
-    if len(address) < 1 : break
+data = uh.read()
 
-    url = serviceurl + urllib.urlencode({'sensor':'false', 'address': address})
-    print 'Retrieving', url
-    uh = urllib.urlopen(url)
-    data = uh.read()
-    print 'Retrieved',len(data),'characters'
-    print data
-    tree = ET.fromstring(data)
+tree = ET.fromstring(data)
+counts = tree.findall('.//count')
+print 'my count:', counts
+print 'my count len:', len(counts)
 
+for item in counts:
+    print item
 
-    results = tree.findall('result')
-    lat = results[0].find('geometry').find('location').find('lat').text
-    lng = results[0].find('geometry').find('location').find('lng').text
-    location = results[0].find('formatted_address').text
-
-    print 'lat',lat,'lng',lng
-    print location
+    #'Count', item.find('count').text
