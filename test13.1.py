d9e0216b8651dd87f6051eb9181e35896f32cc07
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-03-14 01:50:30 +0200

    working

--- test13.1.py
+++ test13.1.py
@@ -8,11 +8,17 @@ uh = urllib.urlopen(url)
 data = uh.read()
 
 tree = ET.fromstring(data)
-counts = tree.findall('.//count')
+counts = tree.findall('.//comment')
 print 'my count:', counts
 print 'my count len:', len(counts)
 
+sum = 0
 for item in counts:
     print item
+    print 'Name', item.find('name').text
+    print 'Count', item.find('count').text
+    count_n = int(item.find('count').text)
+    sum = sum + count_n
 
+print sum
     #'Count', item.find('count').text
