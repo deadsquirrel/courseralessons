e1d02a9c3fed3b4e7fba616900e0daf7ba0a4f9e
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-03-17 00:12:21 +0200

    checkup element of list isdigit

--- pr_test6.1.py
+++ pr_test6.1.py
@@ -7,17 +7,21 @@ print "list:",inp_list
 
 for i in inp_list:
     print i
-    if int(i) == 9:
-        count += 1
-    elif int(i) == 6:
-        count += 1
-    elif int(i) == 0:
-        count += 1
-    elif int(i) == 4:
-        count += 1
-    elif int(i) == 8:
-        count += 2
+    if i.isdigit():
+        if int(i) == 9:
+            count += 1
+        elif int(i) == 6:
+            count += 1
+        elif int(i) == 0:
+            count += 1
+        elif int(i) == 4:
+            count += 1
+        elif int(i) == 8:
+            count += 2
+        else:
+            continue
     else:
+        print "not digit! next i"
         continue
     print "================== "
 print "count hole of list:", count
