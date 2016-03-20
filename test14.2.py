3ca6a8c320f7f83cfe18de016aad90e196f0a229
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-03-20 19:58:21 +0200

    done

--- test14.2.py
+++ test14.2.py
@@ -17,16 +17,9 @@ while True:
     except: js = None
     if 'status' not in js or js['status'] != 'OK':
         print '==== Failure To Retrieve ===='
-        print data
+#        print data
         continue
 
     print json.dumps(js, indent=4)
-
-
-'''
-    lat = js["results"][0]["geometry"]["location"]["lat"]
-    lng = js["results"][0]["geometry"]["location"]["lng"]
-    print 'lat',lat,'lng',lng
-    location = js['results'][0]['formatted_address']
-    print location
-'''
+    pl_id = js["results"][0]["place_id"]
+    print pl_id
