680e6b30925ae3df727987b11b9677441b7c061a
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-03-26 00:04:47 +0200

    clean up

--- my_check.py
+++ my_check.py
@@ -11,15 +11,3 @@ def check_l(nnn):
 #            print 'elem:', elem
             n.append(elem)
     return n
-
-'''
-import re
-def check_l(nnn):
-    for line in nnn:
-        if re.search('[a-zA-Z\._-]+@[a-z\.]+', line):
-            print line
-            p = re.compile('[a-zA-Z\._-]+@[a-z\.]+')
-            n = p.findall(line)
-            return n
-    
-'''
