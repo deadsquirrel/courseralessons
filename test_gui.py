da145a6c355550c516f2425c8bcaead282963771
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-03-29 00:01:07 +0300

    add testing GUI with Tkinter

--- test_gui.py
+++ test_gui.py
@@ -3,3 +3,16 @@ from Tkinter import *
 root = Tk()
 but = Button(root)
 but["text"] = "Печать"
+
+def printer(event):
+    print ("Как всегда очередной 'Hello world!'")
+
+# click left button of mouse - <Button-1>
+# relations (binding) events and function
+but.bind("<Button-1>", printer)
+
+# visibility of button
+but.pack()
+
+# visibility of window
+root.mainloop()
