83accf2ab1f128f83f95bcfaa66ecd44498c054d
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-03-30 23:30:19 +0300

    add example flags

--- prakt_rab.py
+++ prakt_rab.py
@@ -19,10 +19,19 @@ tex = Text(root,width=20,height=6,
           font="Verdana 12",
           wrap=WORD) 
 
+varcomm = Label(root, text="Сколько штук?", font="Arial 14")
+var=IntVar()
+var.set(1)
+rad0 = Radiobutton(root,text="0-10",variable=var,value=0)
+rad1 = Radiobutton(root,text="11-20",variable=var,value=1)
+rad2 = Radiobutton(root,text="21-30",variable=var,value=2) 
+rad3 = Radiobutton(root,text="31-40",variable=var,value=3) 
 
 lab.pack()
 ent.pack()
 comm.pack()
 tex.pack()
+varcomm.pack()
+
 
 root.mainloop()
