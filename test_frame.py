6f46210d148450374b750e4e62b8aeaaa52179a5
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-04-01 20:53:41 +0300

    add test_frame - testing color

--- test_frame.py
+++ test_frame.py
@@ -3,15 +3,22 @@ from Tkinter import *
 root = Tk()
  
 fra1 = Frame(root,width=500,height=100,bg="darkred")
+fra8 = Frame(root,width=500,height=50,bg="red")
 fra2 = Frame(root,width=300,height=200,bg="green",bd=20)
 fra3 = Frame(root,width=500,height=150,bg="darkblue")
 fra4 = Frame(root,width=300,height=50,bg="blue")
 fra5 = Frame(root,width=200,height=100,bg="yellow")
+fra6 = Frame(root,width=200,height=50,bg="darkgreen")
+fra7 = Frame(root,width=200,height=50,bg="darkbraun")
  
 fra1.pack()
+fra8.pack()
 fra2.pack()
+fra6.pack()
 fra3.pack()
 fra4.pack()
 fra5.pack()
+fra7.pack()
+fra9.pack()
 
 root.mainloop() 
