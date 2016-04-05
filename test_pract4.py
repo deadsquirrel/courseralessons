23c3bb96734bd2fc348c4fd64713cc6dd38fa0c2
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-04-05 23:47:57 +0300

    practice

---
+++ test_pract4.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from Tkinter import *
+
+root = Tk()
+
+fra1 = Frame(root,width=350,height=100,bg="light green")
+fra2 = Frame(root,width=150,height=100,bg="dark green")
+
+
+#sca1 = Scale(fra1,orient=HORIZONTAL,length=100,
+#             from_=0,to=100,tickinterval=10,resolution=10)
+#sca2 = Scale(root,orient=VERTICAL,length=400,
+#          from_=1,to=2,tickinterval=0.1,resolution=0.1)
+
+
+fra1.pack()
+fra2.pack()
+#sca1.pack()
+#sca2.pack()
+root.mainloop() 
--- test_scroolbar.py
+++ test_scroolbar.py
@@ -9,4 +9,10 @@ tx.configure(yscrollcommand=scr.set)
  
 tx.grid(row=0,column=0)
 scr.grid(row=0,column=1)
+
+win = Toplevel(root,relief=SUNKEN,bd=10,bg="lightblue")
+win.title("Дочернее окно")
+win.minsize(width=400,height=200) 
+
+
 root.mainloop() 
