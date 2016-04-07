d3db7fff237783c8df4c2bbc5f597792f0c93b8e
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-04-07 23:59:27 +0300

    my toys

---
+++ test_scroolbar2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from Tkinter import *
+ 
+root = Tk()
+ 
+tx = Text(root,width=10,height=20,font='14')
+scr = Scrollbar(root,command=tx.yview)
+tx.configure(yscrollcommand=scr.set)
+
+
+#tx.grid(row=0,column=0)
+#scr.grid(row=0,column=1)
+
+scr.pack(side = TOP, fill=X)
+tx.pack(side = LEFT, fill = BOTH)
+
+root.mainloop()
