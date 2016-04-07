070eb588d2cc561a105813c8bfb76f0cc6aad1da
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-04-07 22:35:31 +0300

    new example and changed scroolbar

---
+++ examples/scroolbar2.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from Tkinter import *
+
+root = Tk()
+scrollbar = Scrollbar(root)
+scrollbar.pack( side = RIGHT, fill=Y )
+
+mylist = Listbox(root, yscrollcommand = scrollbar.set )
+for line in range(100):
+   mylist.insert(END, "This is line number " + str(line))
+
+mylist.pack( side = LEFT, fill = BOTH )
+scrollbar.config( command = mylist.yview )
+
+mainloop()
+
+
+'''
+from Tkinter import *
+ 
+root = Tk()
+ 
+tx = Text(root,width=10,height=10,font='14')
+scr = Scrollbar(root,command=tx.yview)
+tx.configure(yscrollcommand=scr.set)
+ 
+tx.grid(row=0,column=0)
+scr.grid(row=0,column=1)
+
+root.mainloop()
+'''
--- test_scroolbar.py
+++ test_scroolbar.py
@@ -3,11 +3,15 @@ from Tkinter import *
  
 root = Tk()
  
-tx = Text(root,width=10,height=10,font='14')
+tx = Text(root,width=10,height=20,font='14')
 scr = Scrollbar(root,command=tx.yview)
 tx.configure(yscrollcommand=scr.set)
- 
-tx.grid(row=0,column=0)
-scr.grid(row=0,column=1)
+
+
+#tx.grid(row=0,column=0)
+#scr.grid(row=0,column=1)
+
+scr.pack(side = RIGHT, fill=Y)
+tx.pack(side = LEFT, fill = BOTH)
 
 root.mainloop()
