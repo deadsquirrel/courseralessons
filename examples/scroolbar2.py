5736ef818e20121c5cf9f4e20515e9e548a4fbeb
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-04-08 23:28:00 +0300

    testing scroolbar

--- examples/scroolbar2.py
+++ examples/scroolbar2.py
@@ -14,18 +14,3 @@ scrollbar.config( command = mylist.yview )
 
 mainloop()
 
-
-'''
-from Tkinter import *
- 
-root = Tk()
- 
-tx = Text(root,width=10,height=10,font='14')
-scr = Scrollbar(root,command=tx.yview)
-tx.configure(yscrollcommand=scr.set)
- 
-tx.grid(row=0,column=0)
-scr.grid(row=0,column=1)
-
-root.mainloop()
-'''
