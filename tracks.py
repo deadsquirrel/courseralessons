3c0e0af6a29850cdd752e1fe388c4c3562f228c8
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-04-11 01:41:56 +0300

    filling table Genre

--- tracks.py
+++ tracks.py
@@ -34,7 +34,8 @@ CREATE TABLE Track (
         AUTOINCREMENT UNIQUE,
     title TEXT  UNIQUE,
     album_id  INTEGER,
-    len INTEGER, rating INTEGER, count INTEGER
+    len INTEGER, rating INTEGER, count INTEGER, 
+    genre_id INTEGER
 );
 ''')
 
@@ -66,30 +67,32 @@ for entry in all:
     rating = lookup(entry, 'Rating')
     length = lookup(entry, 'Total Time')
     genre = lookup(entry, 'Genre')
+    
     if name is None or artist is None or album is None or genre is None: 
         continue
 
     print name, artist, album, count, rating, length, genre
 
     cur.execute('''INSERT OR IGNORE INTO Artist (name) 
-        VALUES ( ? )''', ( artist, ) )
+    VALUES ( ? )''', ( artist, ) )
     cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
     artist_id = cur.fetchone()[0]
-
+    
     cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
-        VALUES ( ?, ? )''', ( album, artist_id ) )
+    VALUES ( ?, ? )''', ( album, artist_id ) )
     cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
     album_id = cur.fetchone()[0]
-
+    
+    cur.execute('''INSERT OR IGNORE INTO Genre (name) 
+    VALUES ( ? )''', ( genre, ) )
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+    genre_id = cur.fetchone()[0]
+    
     cur.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, len, rating, count) 
-        VALUES ( ?, ?, ?, ?, ? )''', 
-        ( name, album_id, length, rating, count ) )
-
-    cur.execute('''INSERT OR REPLACE INTO Genre
-        (id, genre) 
-        VALUES ( ?, ? )''', 
-        ( id, genre ) )
+    (title, album_id, len, rating, count, genre_id) 
+    VALUES ( ?, ?, ?, ?, ?, ? )''', 
+                ( name, album_id, length, rating, count, genre_id ) )
+    
     
     conn.commit()
 
