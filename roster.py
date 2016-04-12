1df81866ee3cee907e3f8514416b4b9b8952bbfc
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-04-12 23:40:44 +0300

    copy file and add description

--- roster.py
+++ roster.py
@@ -1,7 +1,13 @@
 '''
+This application will read roster data in JSON format, parse the file, and 
+then produce an SQLite database that contains a User, Course, and Member
+table and populate the tables from the data file.
+
 this code is incomplete as I need to modify the program to store the role 
 column in the Member table
 '''
+# -*- coding: utf-8 -*-
+
 import json
 import sqlite3
 
