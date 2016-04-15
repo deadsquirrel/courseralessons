c5ac529bceb8b670efc6b81529a999599655c065
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-04-15 23:43:06 +0300

    effort

--- pr_test5.4.py
+++ pr_test5.4.py
@@ -1,9 +1,11 @@
 # file_search(folder, filename),
 
 import sys
-import pr_test5.4_mod
+import pr_test5_4_mod
 path = int(sys.argv[1])
+print "::", path
 name = int(sys.argv[2])
+print "name = ", name
 
-file_search(path, name)
+pr_test5_4_mod.file_search(path, name)
     
--- pr_test5_4_mod.py
+++ pr_test5_4_mod.py
@@ -1,11 +1,11 @@
 # module for pr_test5.4.py
 # file_search(folder, filename),
 
-def file_search(path, name)
-res = path[0]
-print res
-
-for i in path:
+def file_search(path, name):
+    res = path[0]
+    print res
+'''
+for i in path[1:]:
     if i == name:
         res = res + 'name'
         return res
@@ -13,7 +13,7 @@ for i in path:
         print False
         return False
 
-
+'''
 print 'the end'
 
         
