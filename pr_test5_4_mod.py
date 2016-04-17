fe45e5bb068317a06d0785b649ab6e29cf414964
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-04-17 23:57:41 +0300

    finding name_File in subdir

--- pr_test5_4_mod.py
+++ pr_test5_4_mod.py
@@ -9,8 +9,6 @@ def file_search(path_list, name):
 
 #>>> isinstance(a, list)
 #True
-
-    
     for i in path_list:
         print 'i|', i
         print type(i)
@@ -19,14 +17,22 @@ def file_search(path_list, name):
             print "Yaaaa"
         elif i[0] == "'":
             print "Ooooo"
-        else: print "jjj"    
-        '''
-            if i == name:
-                res = res + '/' + name
-                print 'yes', res
-                break
-            else:
-                print 'no', res
-                continue
-        '''
+            for y in i:
+                print "y==", y
+                if y == name:
+                    pr_test5_4_mod.file_search2(i[1:-1], name)
+                else: break
+        else: print "jjj"
+
+def file_search2 (p, name):
+    print "-----"
+    for j in p:
+        if j == name:
+            res = res + '/' + name
+            print 'yes', res
+            break
+        else:
+            print 'no', res
+            continue
+
     
