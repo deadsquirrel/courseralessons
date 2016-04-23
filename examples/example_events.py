ece969ac4ce960f27b42284f93f3e1ecd54a5fc0
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-04-23 23:56:03 +0300

    example events of keyboard

--- examples/example_events.py
+++ examples/example_events.py
@@ -1,10 +1,12 @@
 # !/usr/bin/python
 # example
-# mouse event
+# mouse events
+# keyboard events
 # -*- coding: utf-8 -*-
 
 
 from Tkinter import *
+# mouse events
 def b1(event):
      root.title("Left mouse-button")
 def b3(event):
@@ -12,11 +14,32 @@ def b3(event):
 def move(event):
      root.title("mouses movies")
  
+# keyboard events
+def exit_(event):
+     root.destroy()
+def caption(event):
+     t = ent.get()
+     lbl.configure(text = t)
+ 
 root = Tk()
+
+# mouse events
 root.minsize(width = 500, height=400)
  
 root.bind('<Button-1>',b1)
 root.bind('<Button-3>',b3)
 root.bind('<Motion>',move)
+ 
+
+
+# keyboard events 
+ent = Entry(root, width = 40)
+lbl = Label(root, width = 80)
+ 
+ent.pack()
+lbl.pack()
+ 
+ent.bind('<Return>',caption)
+root.bind('<Control-z>',exit_)
  
 root.mainloop() 
