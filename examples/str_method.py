9f728b233c64804a6668f6598574fa2d6fa2d312
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-05-04 18:17:49 +0300

    join, find, deliminer, startwith

---
+++ examples/str_method.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env/python
+# -*- coding: utf-8 -*-
+# example
+# startwith, find, join
+# coding ru
+
+
+name = 'Swaroop' # Это объект строки
+
+if name.startswith('Swa'):
+    print('Да, строка начинается на "Swa"')
+
+if 'a' in name:
+    print('Да, она содержит строку "a"')
+
+if name.find('war') != -1:
+    print('Да, она содержит строку "war"')
+
+delimiter = '_*_'
+mylist = ['Бразилия', 'Россия', 'Индия', 'Китай']
+print(delimiter.join(mylist))
