19824b332c5be07f91575a6533a51bd6d121d0a4
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-05-08 23:34:13 +0300

    next little step

--- examples/example_next.py
+++ examples/example_next.py
@@ -1,3 +1,5 @@
+#lesson 11
+
 from Tkinter import *
 from Tkinter.filedialog import *
 import fileinput
@@ -18,5 +20,13 @@ fm.add_command(label="Open...",command=_open)
  
 txt = Text(root,width=40,height=15,font="12")
 txt.pack()
- 
+
+# add
+def _save():
+     sa = asksaveasfilename()
+     letter = txt.get(1.0,END)
+     f = open(sa,"w")
+     f.write(letter)
+     f.close()
+
 root.mainloop() 
