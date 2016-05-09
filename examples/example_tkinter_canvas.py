3138e810d840eded229e2b36388d41776106d3c9
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-05-09 23:52:30 +0300

    add blank for canvas

---
+++ examples/example_tkinter_canvas.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+ 
+from Tkinter import *
+root = Tk()
+
+canv = Canvas(root,width=500,height=500,bg="lightblue",
+          cursor="pencil")
+
+canv.create_line(200,50,300,50,width=3,fill="blue")
+canv.create_line(0,0,100,100,width=2,arrow=LAST)
+
+x=75
+y=110
+canv.create_rectangle(x,y,x+80,y+50,fill="white",outline="blue")
+canv.create_polygon([250,100],[200,150],[300,150],fill="yellow") 
+
+
+canv.create_polygon([250,100],[200,150],[300,150],fill="yellow")
+canv.create_polygon([300,80],[400,80],[450,75],[450,200],
+          [300,180],[330,160],outline="white",smooth=1)
+
+
+canv.create_oval([20,200],[150,300],fill="gray50")
+
+root.mainloop() 
