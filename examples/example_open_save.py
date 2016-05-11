28e94ddc2f0f7f07e6747a441764dde26e3fb932
Yana Ribalchenko <iana.rybalchenko@gmail.com>
2016-05-11 23:53:30 +0300

    add Exit and help to menu

--- examples/example_open_save.py
+++ examples/example_open_save.py
@@ -6,17 +6,26 @@
 from tkinter import *
 from tkinter.filedialog import *
 import fileinput 
+from tkinter.messagebox import *
 
 def _open():
-     op = askopenfilename()
-     for l in fileinput.input(op):
-          txt.insert(END,l)
+    op = askopenfilename()
+    for l in fileinput.input(op):
+        txt.insert(END,l)
+
 def _save():
-     sa = asksaveasfilename()
-     letter = txt.get(1.0,END)
-     f = open(sa,"w")
-     f.write(letter)
-     f.close()
+    sa = asksaveasfilename()
+    letter = txt.get(1.0,END)
+    f = open(sa,"w")
+    f.write(letter)
+    f.close()
+
+def close_win():
+    if askyesno("Exit", "Do you want to quit?"):
+        root.destroy()
+ 
+def about():
+    showinfo("Editor", "This is text editor.\n(test version)")
      
 root = Tk()
 
@@ -27,7 +36,12 @@ fm = Menu(m)
 m.add_cascade(label="File",menu=fm)
 fm.add_command(label="Open...",command=_open)
 fm.add_command(label="Save...",command=_save)
- 
+fm.add_command(label="Exit",command=close_win)
+
+hm = Menu(m)
+m.add_cascade(label="Help",menu=hm)
+hm.add_command(label="About",command=about)
+
 txt = Text(root,width=40,height=15,font="12")
 txt.pack()
 
